75124c5a05d18e84289ab5bacebf7e3bfc077406
tomerwen <30783660+tomerwen@users.noreply.github.com>
2023-03-25 12:45:13 +0300

    Add files via upload

---
+++ maman11.c
@@ -0,0 +1,26 @@
+#include <stdio.h>
+
+
+/*           int strcmp(cs,ct) - compare string cs to string ct; return <0 if cs<ct , 0 if cs==ct and >0 if cs>ct.                  */
+/*           int strncmp(cs,ct,n) - compare at most n charactets of string cs to string ct; return <0 if cs<ct , 0 if cs==ct and >0 if cs>ct.         */
+/*           char strchr(cs,c) - return pointer to first occurrence of c in cs or NULL if not present                         */
+
+
+int main(){
+
+}
+
+
+int my_strcmp(cs,ct){
+/* The goal is to run a loop and to compare cs[i] to ct[i] with ascii and return the value */
+int ascii_cs = ascii_ct = 0;
+int i;
+ 
+}
+int my_strsncmp(cs,ct,n){
+
+}
+char my_strcht(cs,c){
+
+}
+
