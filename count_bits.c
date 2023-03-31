6ecbdf16acd2edc4006ffb8959c061b3cac15a46
Jaanuk32 <tomerwen@users.noreply.github.com>
2023-03-31 16:27:43 +0300

    final test for count_bits

--- count_bits.c
+++ count_bits.c
@@ -3,11 +3,16 @@
 void count_bits(unsigned long n){
 int i;
     unsigned long mask = 1ul << (sizeof(unsigned long) * 8 - 1);
+    int even_bits_count=0;
     for(i=0;i<sizeof(unsigned long)*8;i++){
         putchar((n & mask) ? '1':'0');
+        if(i%2==0 & (n&mask)){
+            even_bits_count++;
+        }
         n<<=1;
     }
     putchar('\n');
+    printf("There are %d even bits that are on." , even_bits_count);
 }
 
 int main(){
