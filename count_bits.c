039ce39e78526118464bde5f49a8f289799ddf6c
Jaanuk32 <tomerwen@users.noreply.github.com>
2023-03-31 16:20:17 +0300

    check how bits looks

--- count_bits.c
+++ count_bits.c
@@ -1,8 +1,19 @@
 #include <stdio.h>
-long main(){
-long ans;
-scanf("%d",&ans);
 
-printf("The number of bits that are on and are on even numbers is " + ans);
-return ans;
+int main(){
+    unsigned long n;
+    printf("Enter an unsigned long integer: ");
+    scanf("%lu", &n);
+    printf("The bits of %lu are:\n", n);
+    count_bits(n);
+    return 0;
+}
+void count_bits(unsigned long n){
+int i;
+    unsigned long mask = 1ul << (sizeof(unsigned long) * 8 - 1);
+    for(i=0;i<sizeof(unsigned long)*8;i++){
+        putchar((n & mask) ? '1':'0');
+        n<<=1;
+    }
+    putchar('\n');
 }
