b40ba30c80a19b430196460a96b72dcc02dd06b5
Jaanuk32 <tomerwen@users.noreply.github.com>
2023-04-01 18:38:33 +0300

    i<3python

--- my_string/my_string.c
+++ my_string/my_string.c
@@ -9,32 +9,15 @@ int main(){
     char s1[80]={0},s2[80]={0};
     int menu;
     printf("Please choose what you would like to do: \n1.strcmp\n2.strncmp\n3.strchr");
-    scanf("%1s",menu);
+    scanf("%d",menu);
     if(menu==1){
         printf("Please enter the first word:\n");
         scanf("%79s", s1);
         printf("Please enter the second word:\n");
         scanf("%79s", s2);
-        printf("The result of strcmp on %s and %s is %d.\n",s1,s2,my_strchr(s1,s2));
+        printf("The result of strcmp on %s and %s is %d.\n",s1,s2,my_strcmp(s1,s2));
     }
-    // if(menu==2){
-    //     char num;
-    //     printf("Please enter the first word:\n");
-    //     scanf("%79s", s1);
-    //     printf("Please enter the second word:\n");
-    //     scanf("%79s", s2);
-    //     printf("Please enter a number:\n");
-    //     scanf("%79s",num);
-    //     printf("The result of strncmp of %c amd %c and %c is %c:\n",s1,s2,num,my_strncmp(s1,s2,num));
-    // }
-    // if(menu==3){
-    //     char chr;
-    //     printf("Please enter a word\n");
-    //     scanf("%79s",s1);
-    //     printf("Please enter a char");
-    //     scanf("%79s",chr);
-    //     printf("The result of strchr of %c with %c is %c",s1,chr,my_strchr(s1,chr));
-    // }
+
     return 0;
 }
 
@@ -52,12 +35,5 @@ int my_strcmp(char* cs,char* ct){
     return 0;
 }
 
-// int my_strncmp(cs,ct,n){
 
-// }
-// char my_strchr(cs,c){
-// int MAX_INPUT=80;
-// int my_strchr=-1,i=0; /*this will be the returned result*/
-
-// }
 
