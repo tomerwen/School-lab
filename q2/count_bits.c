180c1d48ac2b14eb4fb5b6d65d205a776b23d366
Jaanuk32 <tomerwen@users.noreply.github.com>
2023-04-01 17:52:34 +0300

    San

--- q2/count_bits.c
+++ q2/count_bits.c
@@ -1,32 +1,30 @@
 #include <stdio.h>
 
-void count_bits(unsigned long n){
-int i;
-    unsigned long mask = 1ul << (sizeof(unsigned long) * 8 - 1);
-    int even_bits_count=0;
-    for(i=0;i<sizeof(unsigned long)*8;i++){
-        putchar((n & mask) ? '1':'0');
-        if(i % 2 == 0 && (n & mask)){
-            even_bits_count++;
+int count_bits(unsigned long n){
+    int i = 0, ret = 0;
+    unsigned long x = 0;
+    for (i = 0; i < 64; i ++){
+        x = n & 1;
+        if (x){
+            ret++;
         }
-        n<<=1;
+        n >>= 2;
     }
-    putchar('\n');
-    printf("There are %d even bits that are on.\n" , even_bits_count);
+    return ret;
 }
-
-int main(){
-    unsigned long n;
+int main()
+{
+    unsigned long x;
     printf("-----------------------------------------------------------------------------------");
     printf("Enter an unsigned long integer: \n");
     while(1){
-        if(scanf("%lu", &n)==1){
-        printf("The bits of %lu are:\n", n);
-        count_bits(n);
-        } else{
-            printf("End of input.");
-            break;
+            if(scanf("%lu", &n)==1){
+            printf("The bits of %lu are:\n", n);
+            count_bits(n);
+            } else{
+                printf("End of input.");
+                break;
+                }
             }
-        }
-        return 0;
+            return 0;
 }
