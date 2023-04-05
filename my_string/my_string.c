c9df7d6f7347cbab647b6b7d9004ca27f2c6aadd
Jaanuk32 <tomerwen@users.noreply.github.com>
2023-04-05 14:08:38 +0300

    test on fixing warning on compilation

--- my_string/my_string.c
+++ my_string/my_string.c
@@ -5,22 +5,6 @@
 /*           char strchr(cs,c) - return pointer to first occurrence of c in cs or NULL if not present                        */
 
 
-int main(){
-    char s1[80]={0},s2[80]={0};
-    int menu,ans;
-    printf("Please choose what you would like to do: \n1.strcmp\n2.strncmp\n3.strchr\n");
-    ans = scanf("%d",&menu);
-    while(ans!=EOF){
-        if(menu==1){
-            printf("Please enter the first word:\n");
-            scanf("%79s", s1);
-            printf("Please enter the second word:\n");
-            scanf("%79s", s2);
-            printf("The result of strcmp on %s and %s is %d.\n",s1,s2,my_strcmp(s1,s2));
-        }
-    }
-    return 0;
-}
 
 
 int my_strcmp(char* cs,char* ct){
@@ -45,6 +29,23 @@ int my_strncmp(char* cs,char* ct,int n){
             else if (cs[i]<ct[i]){
                 return -1;
             }
+        }
+    }
+    return 0;
+}
+
+int main(){
+    char s1[80]={0},s2[80]={0};
+    int menu,ans;
+    printf("Please choose what you would like to do: \n1.strcmp\n2.strncmp\n3.strchr\n");
+    ans = scanf("%d",&menu);
+    while(ans!=EOF){
+        if(menu==1){
+            printf("Please enter the first word:\n");
+            scanf("%79s", s1);
+            printf("Please enter the second word:\n");
+            scanf("%79s", s2);
+            printf("The result of strcmp on %s and %s is %d.\n",s1,s2,my_strcmp(s1,s2));
         }
     }
     return 0;
