83c326fb2243532c9bb361792a4bca1a1c39b1c9
Jaanuk32 <tomerwen@users.noreply.github.com>
2023-04-27 14:04:40 +0300

    started mmn12

---
+++ mmn 12/my_set.c
@@ -0,0 +1,18 @@
+#include <stdio.h>
+
+#define ENLARGE_SIZE = 5;
+
+int get_set(){ /* recieve input from user and build group*/
+    return 1;
+}
+
+int print_set(){ /* print the set in orginized way*/
+    return 1;
+}
+
+int main(){ /*run the program*/
+printf("Please enter your list of integers");
+
+    get_set();
+    print_set();
+}
