779c757a7e8d8b29ae6b12750065f9cd57fd4524
Jaanuk32 <tomerwen@users.noreply.github.com>
2023-05-05 21:37:12 +0300

    testing if works

--- mmn 12/my_set.c
+++ mmn 12/my_set.c
@@ -1,18 +1,41 @@
 #include <stdio.h>
 
-#define ENLARGE_SIZE = 5;
-
-int get_set(){ /* recieve input from user and build group*/
-    return 1;
+#define ENLARGE_SIZE realloc(set,5*sizeof(int));
+/* needs to be used on realloc function*/
+void get_set(){ /* recieve input from user and build group*/
+    int arraysize = 5;
+    printf("Please enter your inputs for the set");
+    int input;
+    int j=0;
+    int *set[5];
+    while(input!=EOF){
+    scanf("%d",&input);
+    for(int i=0;i<=j;i++){
+        if(j==0){
+            set[0]=input;
+            j++;
+        }
+        if (j==arraysize){
+            ENLARGE_SIZE;
+            arraysize=arraysize+5;
+        }
+        if(set[i]==input){
+            break;
+        }
+        set[j+1]==input;
+    }
+    }
+    print_set(set,arraysize);
 }
 
-int print_set(){ /* print the set in orginized way*/
-    return 1;
+void print_set(int set[], int size){ /* print the set in orginized way*/
+    for(int i=0;i<size;i++){
+        printf("%d", set[i]);
+    }
 }
 
 int main(){ /*run the program*/
 printf("Please enter your list of integers");
 
     get_set();
-    print_set();
 }
