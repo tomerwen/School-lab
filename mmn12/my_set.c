cc09ebfc8fe2a57145506a63fd58ac5a19159ac6
Jaanuk32 <tomerwen@users.noreply.github.com>
2023-05-07 18:31:28 +0300

    real third

--- mmn12/my_set.c
+++ mmn12/my_set.c
@@ -1,19 +1,19 @@
 #include <stdio.h>
 #include <stdlib.h>
 
-#define ENLARGE_SIZE set = realloc(set, (arraysize + 5) * sizeof(int))
+#define ENLARGE_SIZE set = (int*)realloc(set, (arraysize+5) * sizeof(int))
 int *set;
 int arraysize = 5;
 
-void print_set() {
+void print_set() { /* print the set in orginized way*/
+    int i;
     printf("This is the set: ");
-    for (int i = 0; i < arraysize; i++) {
+    for (i = 0; i < arraysize; i++){
         printf("%d", set[i]);
-        if (i < arraysize - 1) {
+        if (i < arraysize-1){
             printf(", ");
         }
     }
-    printf("\n");
 }
 
 void get_set() {
@@ -36,14 +36,12 @@ void get_set() {
             set[j++] = input;
         }
     }
+    set = (int*)realloc(set, j * sizeof(int));
+    arraysize = j;
 }
 
-int main() {
-    set = malloc(arraysize * sizeof(int));
-    if (set == NULL) {
-        printf("Memory allocation error\n");
-        return 1;
-    }
+int main(){ /*run the program*/
+    set = (int*)malloc(arraysize * sizeof(int));
     get_set();
     print_set();
     free(set);
