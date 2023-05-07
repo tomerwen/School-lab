208591515debeb884363e4296c4ea1b41fd49b01
Jaanuk32 <tomerwen@users.noreply.github.com>
2023-05-07 17:53:19 +0300

    gpt test

--- mmn12/my_set.c
+++ mmn12/my_set.c
@@ -1,46 +1,40 @@
 #include <stdio.h>
 #include <stdlib.h>
 
-#define ENLARGE_SIZE realloc(*set,5*sizeof(int));
+#define ENLARGE_SIZE realloc(set,5*sizeof(int));
 int set[5];
 int arraysize = 5;
 
-void print_set(){ /* print the set in orginized way*/
+void print_set() { /* print the set in orginized way*/
     int i;
-    for(i=0;i<=arraysize;i++){
+    for (i = 0; i <= arraysize; i++){
         printf("%d", set[i]);
-        if(i<arraysize){
-        printf(", ");
+        if (i < arraysize){
+            printf(", ");
         }
-        }
-}
-void get_set(){ /* recieve input from user and build group*/
-    int input=0;
-    int j=0;
-
-    printf("Please enter your inputs for the set");
-    while(input!=EOF){
-    int i;
-    scanf("%d",&input);
-    for(i=0;i<=j;i++){
-        printf("%d",i);
-        if(j==0){
-            set[0]=input;
-            j++;
-        }
-        if (j==arraysize){
-            ENLARGE_SIZE;
-            arraysize=arraysize+5;
-        }
-        if(set[i]==input){
-            break;
-        }
-        set[j]=input;
-        j++;
-    }
     }
 }
+void get_set() {
+    int input;
+    int j = 0;
 
+    printf("Please enter your inputs for the set: ");
+    while (scanf("%d", &input) == 1) {
+        int i;
+        for (i = 0; i < j; i++) {
+            if (set[i] == input) {
+                break;
+            }
+        }
+        if (i == j) {
+            set[j++] = input;
+            if (j == arraysize) {
+                ENLARGE_SIZE;
+                arraysize += 5;
+            }
+        }
+    }
+}
 
 int main(){ /*run the program*/
     get_set();
