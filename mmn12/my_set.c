f9564b9b7fe1863000e5996863cd726573b742bc
Jaanuk32 <tomerwen@users.noreply.github.com>
2023-05-07 18:15:53 +0300

    realloc to amount of inputs

--- mmn12/my_set.c
+++ mmn12/my_set.c
@@ -1,12 +1,13 @@
 #include <stdio.h>
 #include <stdlib.h>
 
-#define ENLARGE_SIZE set=realloc(set,1*sizeof(int));
+#define ENLARGE_SIZE set=realloc(set,5*sizeof(int));
 int *set;
 int arraysize = 5;
 
 void print_set() { /* print the set in orginized way*/
     int i;
+    printf("This is the set: ");
     for (i = 0; i <= arraysize; i++){
         printf("%d", set[i]);
         if (i < arraysize){
@@ -18,7 +19,7 @@ void get_set() {
     int input;
     int j = 0;
 
-    printf("Please enter your inputs for the set: ");
+    printf("Please enter your inputs for the set: \n");
     while (scanf("%d", &input) == 1) {
         int i;
         for (i = 0; i < j; i++) {
@@ -34,6 +35,8 @@ void get_set() {
             set[j++] = input;
         }
     }
+    set = realloc(set, j * sizeof(int));
+    arraysize = j;
 }
 
 int main(){ /*run the program*/
